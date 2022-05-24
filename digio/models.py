b22df3067e18a3bc744dcd2468660edfe0d1f8d8
ΑΛΕΞΑΝΔΡΟΣ ΙΩΑΝΝΙΔΗΣ <ioalexandros@hotmail.com>
2022-05-24 15:19:14 +0300

    first model

--- digio/models.py
+++ digio/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class Tag(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	name = models.CharField(max_length=150, null=True)
+
+	def __str__(self):
+		return self.name
