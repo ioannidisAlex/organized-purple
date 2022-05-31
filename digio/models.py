4265d7b0302e6f3ab07cccd4b77d04ac4e19971e
ΑΛΕΞΑΝΔΡΟΣ ΙΩΑΝΝΙΔΗΣ <ioalexandros@hotmail.com>
2022-05-31 16:33:45 +0300

    DurationIn projectnGrant

--- digio/models.py
+++ digio/models.py
@@ -9,3 +9,31 @@ class Tag(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Manager(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	name = models.CharField(max_length=30)
+
+class Program(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	title = models.CharField(max_length=50)
+	manager = models.ForeignKey(
+		Manager, on_delete = models.CASCADE
+	)
+
+class ProjectnGrant(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	grant = models.IntegerField()
+	general_program = models.ForeignKey(
+		Program, models.SET_NULL, blank=True, null=True
+	)
+	#another entity for many to many
+	#so that for every connection there is one
+	#connection with a new class object connection
+	tags = models.ManyToManyField(Tag)
+	#duration = models.DurationField()
+	start_time = models.DateTimeField(null=False)
+	end_time = models.DateTimeField(null=False)
+	
+	def duration(self):
+		return self.end_time - self.start_time
